05872f8d3e9a2a7b6be02417b1deac46ef63018c
Saptarshi Sarkar <saptarshi2017cob@gmail.com>
2021-05-16 00:33:00 +0530

    first

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+i=int(input("enter a number : "))
+k=i
+while(i):
+    print(" hello world "*(k-i))
+    i=i-1
+
+n = input("Enter any value to terminate the program : ")
+# pure python code
+# exe file !!
